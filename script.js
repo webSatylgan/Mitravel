f4d905c81cddadf85ba6dbc28d3103b18b89278b
Danial-github <82264689+Danial-github@users.noreply.github.com>
2021-09-24 10:21:34 +0600

    Add files via upload

--- script.js
+++ script.js
@@ -3,17 +3,6 @@ const swiper = new Swiper('.swiper', {
     loop: true,
     centeredSlides: true,
     spaceBetween: 100,
-
-    breakpoints: {
-        // when window width is >= 320px
-        1000: {
-          spaceBetween: 120
-        },
-
-        500: {
-          spaceBetween: 60
-        },
-    },
-
-
 });
+
+lazyload();
